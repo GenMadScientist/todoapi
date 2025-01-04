a6d96fc1225555e72df5292119bb4a9721c719b6
genuis001 <razakabu626@gmail.com>
2025-01-04 17:58:29 +0100

    add database and deployment seetings

--- todoapi/settings.py
+++ todoapi/settings.py
@@ -9,12 +9,14 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
+from dotenv import load_dotenv
 from datetime import timedelta
+import dj_database_url
 
 import os
 
 from pathlib import Path
-
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zvm@%3%1ec0*w4bcs+^mglkxd%my&4c9jofy76vtg8t$1!np63'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "True").lower() == "true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -80,13 +82,17 @@ WSGI_APPLICATION = 'todoapi.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
+#postgresql://postgres:UskQcXYuoivKbKgLfcYFQyyccUOCQwYA@viaduct.proxy.rlwy.net:47896/railway
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+if not DEBUG:
+    DATABASES = {
+        'default': dj_database_url.parse("postgresql://postgres:UskQcXYuoivKbKgLfcYFQyyccUOCQwYA@viaduct.proxy.rlwy.net:47896/railway")
+    }
 
 
 # Password validation
